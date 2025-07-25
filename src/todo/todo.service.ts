10db2cb7a96746ad22c719a7d867bad4bc3630f1
Soumya Biswas <biswassoumya17@gmail.com>
2025-07-25 21:01:54 +0530

    fix issue in delete

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,47 +4,52 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
-  constructor(private readonly databaseService: DatabaseService) {}
+    constructor(private readonly databaseService: DatabaseService) { }
 
-  async fetchAllTodo() {
-    const fetchData = await this.databaseService.todo.findMany();
-    return fetchData;
-  }
+    async fetchAllTodo() {
+        const fetchData = await this.databaseService.todo.findMany({
+            where: {
+                deletedAt: null,
+            },
+        });
+        return fetchData;
+    }
 
-  async fetchTodoById(id: string) {
-    const fetchData = await this.databaseService.todo.findFirst({
-      where: { id: id },
-    });
 
-    return fetchData;
-  }
+    async fetchTodoById(id: string) {
+        const fetchData = await this.databaseService.todo.findFirst({
+            where: { id, deletedAt: null },
+        });
+        return fetchData;
+    }
 
-  async createTodo(createTodoDto: CreateTodoDto) {
-    const res = await this.databaseService.todo.create({
-      data: {
-        title: createTodoDto.title,
-        description: createTodoDto.description,
-      },
-    });
-    return { res };
-  }
+    async createTodo(createTodoDto: CreateTodoDto) {
+        const res = await this.databaseService.todo.create({
+            data: {
+                title: createTodoDto.title,
+                description: createTodoDto.description,
+            },
+        });
+        return { res };
+    }
 
-  async updateTodo(createTodoDto: CreateTodoDto, id: string) {
-    const updateById = await this.databaseService.todo.update({
-      where: { id: id },
-      data: {
-        title: createTodoDto.title,
-        description: createTodoDto.description,
-      },
-    });
-    return updateById;
-  }
+    async updateTodo(createTodoDto: CreateTodoDto, id: string) {
+        const updateById = await this.databaseService.todo.update({
+            where: { id: id },
+            data: {
+                title: createTodoDto.title,
+                description: createTodoDto.description,
+            },
+        });
+        return updateById;
+    }
 
-  async deleteById(id: string) {
-    const response = await this.databaseService.todo.findFirst({
-      where: { id: id },
-    });
+    async deleteById(id: string) {
+        const response = await this.databaseService.todo.delete({
+            where: { id },
+            data: { deletedAt: new Date() },
+        });
+        return response;
+    }
 
-    return response;
-  }
 }
